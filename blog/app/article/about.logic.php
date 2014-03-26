667c623d2f67da9b8a0b7c4e0840f8adcf42f9e5
狮子的魂 <chenxin619315@gmail.com>
2014-03-26 19:16:35 +0800

    logic file handler demo

---
+++ blog/app/article/about.logic.php
@@ -0,0 +1,11 @@
+<?php
+$this->view->assoc('about', $this->model->getItem());
+		
+		//get the executed html content
+		$_html = $this->view->getHtml('about.html');
+		
+		$this->output->compress(9);		//set the compress level
+		$this->output->display($_html);
+		
+		$this->uri->redirect('article/list');
+?>
