e4d7700864c7981d046cc0aa757328250cae908a
chenxin <chenxin619315@gmail.com>
2016-02-26 17:07:36 +0800

    add loader package support

--- syrian/core/Syrian.merge.php
+++ syrian/core/Syrian.merge.php
@@ -142,18 +142,19 @@ class Loader
      * The function will check script file $_path.class.php first
      *     and then $_path.php
      *
-     * @param    $_class
+     * @param   $_class
      * @param   $_section
-     * @param    $_inc    If $_inc is TRUE check the syrian/lib  or check APPPATH/lib
-     * @return    bool    true for loaded successfuly and false for not
+     * @param   $_inc   If $_inc is TRUE check the syrian/lib  or check APPPATH/lib
+     * @param   $_exit  exit for not found the specifield class
+     * @return  bool    true for loaded successfully and false for not
      */
-    public static function import($_class, $_section = NULL, $_inc = true)
+    public static function import($_class, $_section=NULL, $_inc=true, $_exit=true)
     {
         //All the loaded classes.
         static $_loaded = array();
         
         //$_class = ucfirst($_class);
-        $_cls = ($_section == NULL) ? $_class : $_section . '/' . $_class;
+        $_cls = ($_section == NULL) ? $_class : str_replace('.', '/', $_section).'/'.$_class;
         if ( isset($_loaded[$_cls]) ) return;
         
         //Look for the class in the SYSPATH/lib folder if $_inc is TRUE
@@ -169,7 +170,11 @@ class Loader
             }
         }
         
-        exit('Syrian:Loader#import: Unable to load class ' . $_class);
+        if ( $_exit ) {
+            exit('Syrian:Loader#import: Unable to load class ' . $_class);
+        }
+
+        return false;
     }
     
     
@@ -177,19 +182,23 @@ class Loader
      * function to load data from the specified file
      *     and return the return of the included file as the final result
      *
-     * @param    $_config
-     * @param    $_section
+     * @param   $_config
+     * @param   $_section
      * @param   $_inc   True for seach files in syrian/config
-     * @param    $_key    specifield key
-     * @return    mixed(Array, Object, Bool)
+     * @param   $_key   specifield key
+     * @Param   $_exit  exit for not found the config file ? 
+     * @return  mixed(Array, Object, Bool)
      */
-    public static function config( $_config, $_section=NULL, $_inc=false, $key=NULL )
+    public static function config($_config, $_section=NULL, $_inc=false, $key=NULL, $_exit=true)
     {
         //make the included file name
         $_dir = (($_inc) ? BASEPATH . '/config/' : SR_CONFPATH);
         
         //append the section
-        if ( $_section != NULL ) $_dir .= $_section . '/';
+        if ( $_section != NULL ) {
+            $_dir .= str_replace('.', '/', $_section) . '/';
+        }
+
         $_dir .= $_config;
         
         //search the config file and include it
@@ -197,7 +206,6 @@ class Loader
             if ( file_exists($_file) ) {
                 //return include $_file;
                 $conf = include $_file;
-                
                 if ( $key != NULL ) {
                     return isset($conf["{$key}"]) ? $conf["{$key}"] : NULL;
                 }
@@ -207,18 +215,23 @@ class Loader
         }
         
         //throw new Exception('No such file or directory');
-        exit('Syrian:Loader#config: Unable to load config ' . $_config);
+        if ( $_exit ) {
+            exit('Syrian:Loader#config: Unable to load config ' . $_config);
+        }
+
+        return false;
     }
     
     /**
      * function to load the specifile model maybe from the
      *         specifile path and return the instance of the model
      *
-     * @param    $_model
-     * @param    $_section
-     * @return    Object
+     * @param   $_model
+     * @param   $_section
+     * @param   $_exit exit for not found the model
+     * @return  Object
     */
-    public static function model( $_model, $_section = NULL )
+    public static function model($_model, $_section=NULL, $_exit=true)
     {
         //loaded model
         static $_loaded = array();
@@ -226,7 +239,7 @@ class Loader
         $_model = ucfirst($_model);
         
         //check the loaded of the class
-        $_cls = ($_section == NULL) ? $_model : $_section . '/' . $_model;
+        $_cls = ($_section == NULL) ? $_model : str_replace('.', '/', $_section).'/'.$_model;
         if ( isset( $_loaded[$_cls] ) ) {
             return $_loaded[$_cls];
         }
@@ -253,26 +266,33 @@ class Loader
             }
         }
         
-        exit('Syrain:Loader#model: Unable to load model ' . $_model);
+        if ( $_exit ) {
+            exit('Syrain:Loader#model: Unable to load model ' . $_model);
+        }
+
+        return false;
     }
 
     /**
      * function to load and create helper instance
      *
-     * @param    $_helper
-     * @param    $_section
+     * @param   $_helper
+     * @param   $_section
      * @param   $_inc   True for seach files in syrian/helper
-     * @param    $_conf    configuration to create the instance
-     * @return    mixed(Array, Object, Bool)
+     * @param   $_conf  configuration to create the instance
+     * @param   $_exit  exit for not found the helper
+     * @return  mixed(Array, Object, Bool)
      */
-    public static function helper($_helper, $conf=NULL, $_section = NULL, $_inc = false)
+    public static function helper($_helper, $conf=NULL, $_section=NULL, $_inc=false, $_exit=true)
     {
         //All the loaded helper.
         static $_loaded = array();
         
         //$_class = ucfirst($_class);
-        $_cls = ($_section == NULL) ? $_helper : $_section . '/' . $_helper;
-        if ( isset($_loaded[$_cls]) ) return $_loaded[$_cls];
+        $_cls = ($_section == NULL) ? $_helper : str_replace('.', '/', $_section).'/'.$_helper;
+        if ( isset($_loaded[$_cls]) ) {
+            return $_loaded[$_cls];
+        }
         
         //Look for the class in the SYSPATH/lib folder if $_inc is TRUE
         //Or check the APPPATH/lib 
@@ -282,14 +302,18 @@ class Loader
         foreach( array($_dir . '.helper.php', $_dir . '.php') as $_file ) {
             if ( file_exists($_file) ) {
                 require $_file;
-                $_class    = $_helper.'Helper';
-                $obj = new $_class($conf);
+                $_class = $_helper.'Helper';
+                $obj    = new $_class($conf);
                 $_loaded[$_cls] = &$obj;
                 return $obj;
             }
         }
         
-        exit('Syrian:Loader#helper: Unable to load helper ' . $_helper);
+        if ( $_exit ) {
+            exit('Syrian:Loader#helper: Unable to load helper ' . $_helper);
+        }
+
+        return false;
     }
 }
 
