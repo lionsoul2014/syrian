cd3a21451dfbe440e9f2594615972e008898d87d
lionsoul <chenxin619315@gmail.com>
2016-05-21 11:52:40 +0800

    make the cache file ext be optional

--- syrian/lib/cache/NFileCache.class.php
+++ syrian/lib/cache/NFileCache.class.php
@@ -6,35 +6,34 @@
 */
 class NFileCache implements ICache
 {
-
-    private $_length     = 2000;
-    public     $_cache_dir = NULL;
-    public    $_cache_f    = NULL;
-    public    $upt        = false;
+    private $_length    = 2000;
+    public  $_file_ext  = '.cache';
+    public  $_cache_dir = NULL;
+    public  $_cache_f   = NULL;
+    public  $upt        = false;
 
     public $_baseKey    = NULL;
-    public $_factor        = NULL;
-    public $_fname        = NULL;
+    public $_factor     = NULL;
+    public $_fname      = NULL;
     public $_ttl        = 0;
     
-    public function __construct( $_args = NULL )
+    public function __construct( $_args=NULL )
     {
-        if ( $_args != NULL ) 
-        {
-            if ( isset($_args['cache_dir']) )    $this->_cache_dir = $_args['cache_dir'];
-            if ( isset($_args['length']) )        $this->_length = $_args['length'];
+        if ( $_args != NULL ) {
+            if ( isset($_args['cache_dir']) )   $this->_cache_dir = $_args['cache_dir'];
+            if ( isset($_args['length']) )      $this->_length = $_args['length'];
+            if ( isset($_args['file_ext']) )    $this->_file_ext = $_args['file_ext'];
         }
     }
 
     //set the baseKey
     public function baseKey( $_baseKey )
     {
-        if ( $this->_baseKey == $_baseKey ) 
-        {
+        if ( $this->_baseKey == $_baseKey ) {
             return $this;
         }
 
-        $this->_baseKey    = $_baseKey;
+        $this->_baseKey = $_baseKey;
         $this->upt = true;
         return $this;
     }
@@ -42,12 +41,11 @@ class NFileCache implements ICache
     //set the Factor
     public function factor( $_factor )
     {
-        if ( $_factor == $this->_factor )
-        {
+        if ( $_factor == $this->_factor ) {
             return $this;
         }
 
-        $this->_factor    = $_factor;
+        $this->_factor = $_factor;
         $this->upt = true;
         return $this;
     }
@@ -55,12 +53,11 @@ class NFileCache implements ICache
     //set the file name
     public function fname( $_fname )
     {
-        if ( $_fname == $this->_fname )
-        {
+        if ( $_fname == $this->_fname ) {
             return $this;
         }
 
-        $this->_fname    = $_fname;
+        $this->_fname = $_fname;
         $this->upt = true;
         return $this;
     }
@@ -75,25 +72,23 @@ class NFileCache implements ICache
     private function getCacheFile()
     {
         if ( $this->upt == false 
-            && $this->_cache_f != NULL ) 
-        {
+            && $this->_cache_f != NULL ) {
             return $this->_cache_f;
         }
 
         //convert the baseKey to cache path
         $path = $this->_cache_dir.str_replace('.', '/', $this->_baseKey);
 
-        if ( $this->_factor == NULL )
+        if ( $this->_factor == NULL ) {
             $_file = ($this->_fname==NULL) ? 'default' : $this->_fname;
-        else
-        {
+        } else {
             $path = $path.'/'.floor(($this->_factor / $this->_length));
             if ( $this->_fname == null ) $_file = ($this->_factor % $this->_length);
             else $_file = "{$this->_fname}";
         }
 
         //return the cache file path
-        $this->_cache_f    = $path.'/'.$_file.'.cache';
+        $this->_cache_f = $path.'/'.$_file.$this->_file_ext;
         return $this->_cache_f;
     }
 
@@ -116,8 +111,7 @@ class NFileCache implements ICache
         
         $path = dirname($_cache_file);
         //check and make the $path dir
-        if ( ! file_exists( $path ) )
-        {
+            if ( ! file_exists( $path ) ) {
             $_dir = dirname( $path );
             $_names = array();
             do {
@@ -126,11 +120,11 @@ class NFileCache implements ICache
                 $_dir = dirname( $_dir );
             } while ( true );
             
-            for ( $i = count($_names) - 1; $i >= 0; $i-- )
-            {
+            for ( $i = count($_names) - 1; $i >= 0; $i-- ) {
                 $_dir = $_dir.'/'.$_names[$i];
                 mkdir($_dir, 0777);
             }
+
             mkdir($path, 0777);
         }
         
