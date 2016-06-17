77740f48f6fd5b5c01cb92cdcdaab816657ce1ca
lionsoul <>
2016-06-17 21:44:49 +0800

    add import,config,model,helper functions

--- syrian/core/kernel/Function.php
+++ syrian/core/kernel/Function.php
@@ -105,4 +105,213 @@ if ( ! function_exists('_cli_initialize') ) {
 
     }
 }
+
+/**
+ * import the specifield class
+ *
+ * @param   $cls_path class path like db.dbFactory
+ * @param   $_inc If $_inc is True check the syrian/lib or check under SR_LIBPATH
+*/
+if ( ! function_exists('import') ) {
+    function import($cls_path, $_inc=true)
+    {
+        static $_loadedClass = array();
+
+        $path = str_replace('.', '/', $cls_path);
+        if ( isset($_loadedClass[$path]) ) {
+            unset($path);
+            return true;
+        }
+        
+        //Look for the class in the SYSPATH/lib folder if $_inc is TRUE
+        //Or check the SR_LIBPATH
+        $_dir   = (($_inc) ? BASEPATH . 'lib/' : SR_LIBPATH);
+        $_dir  .= $path;
+        $found  = false;
+
+        foreach ( array($_dir . '.class.php', $_dir . '.php') as $_file ) {
+            if ( file_exists($_file) ) {
+                require $_file;
+                $_loadedClass[$path] = true;
+                $found = true;
+                break;
+            }
+        }
+
+        unset($path, $_dir);
+        if ( $found == true ) {
+            return true;
+        }
+        
+        throw new Exception("import#Unable to load class with path {$cls_path}");
+    }
+}
+
+/**
+ * import the specifield configuration
+ * and return the return of the included file as the final result
+ *
+ * @param   $config_path
+ * @param   $_inc @see #import
+ * @param   $cache cache the file incldue ?
+ * @usage
+ * $conf = config('db.hosts');
+ * $mysql = config('db.hosts#mysql');
+*/
+if ( ! function_exists('config') ) {
+    function config($config_path, $_inc=false, $cache=true)
+    {
+        static $_loadedConf = array();
+
+        if ( ($sIdx = strpos($config_path, '#')) !== false ) {
+            $path = str_replace('.', '/', substr($config_path, 0, $sIdx));
+            $keys = explode('.', substr($config_path, $sIdx + 1));
+        } else {
+            $path = str_replace('.', '/', $config_path);
+            $keys = NULL;
+        }
+
+        //check and load the configure
+        $found = true;
+        if ( ! isset($_loadedConf[$path]) || $cache == false ) {
+            $_dir  = (($_inc) ? BASEPATH . '/config/' : SR_CONFPATH);
+            $_dir .= $path;
+            $found = false;
+
+            //search the config file and include it
+            foreach ( array($_dir . '.conf.php', $_dir . '.php' ) as $_file ) {
+                if ( file_exists($_file) ) {
+                    $found = true;
+                    $_loadedConf[$path] = include $_file;
+                    break;
+                }
+            }
+            
+            unset($_dir);
+        }
+
+        if ( $found == false ) {
+            unset($path, $keys);
+            throw new Exception("config#Unable to load configure with path {$config_path}");
+        }
+
+        if ( $keys == NULL ) {
+            return $_loadedConf[$path];
+        }
+
+        $vals = $_loadedConf[$path];
+        foreach ( $keys as $key ) {
+            if ( ! isset($vals[$key]) ) {
+                throw new Exception("config#Invalid key {$key}");
+            }
+
+            $vals = $vals[$key];
+        }
+
+        unset($path, $keys);
+        return $vals;
+    }
+}
+
+/**
+ * load the specified model with the given path
+ *
+ * @param   $model_path
+ * @param   $cache check the cache first ?
+ * @return  Object the object of the loaded model 
+*/
+if ( ! function_exists('model') ) {
+    function model($model_path, $cache=true)
+    {
+        static $_loadedModel = array();
+
+        if ( $cache == true 
+            && isset($_loadedModel[$model_path]) ) {
+            return $_loadedModel[$model_path];
+        }
+
+        if ( ($sIdx = strrpos($model_path, '.')) !== false ) {
+            $path  = str_replace('.', '/', substr($model_path, 0, $sIdx));
+            $model = substr($model_path, $sIdx + 1);
+        } else {
+            $path  = NULL;
+            $model = $model_path;
+        }
+        
+        //model base directory
+        $_dir = SR_MODELPATH . $model_path;
+        foreach ( array( $_dir . '.model.php', $_dir . '.php' ) as $_file ) {
+            if ( file_exists( $_file ) ) {
+                if ( ! isset($_loadedModel[$model_path]) ) {
+                    include $_file;
+                }
+
+                $class = "{$model}Model";
+                if ( class_exists($class) )  {
+                    $obj = new $class();
+                } else {
+                    $obj = new $model();
+                }
+
+                //mark loaded for the current model
+                $_loadedModel[$model_path] = $obj;
+                unset($path, $model, $_dir, $class);
+                return $obj;
+            }
+        }
+        
+        throw new Exception("model#Unable to load model with path {$model}");
+    }
+}
+
+/**
+ * load and create then return the specified helper
+ *
+ * @param   $helper_path
+ * @param   $conf
+ * @param   $_inc @see #import
+ * @param   $cache cache the instance ?
+ * @return  Object
+*/
+if ( ! function_exists('helper') ) {
+    function helper($helper_path, $conf=NULL, $_inc=false, $cache=true)
+    {
+        static $_loadedHelper = array();
+
+        if ( $cache == true 
+            && isset($_loadedHelper[$helper_path]) ) {
+            return $_loadedHelper[$helper_path];
+        }
+
+        if ( ($sIdx = strrpos($helper_path, '.')) !== false ) {
+            $path   = str_replace('.', '/', substr($helper_path, 0, $sIdx));
+            $helper = substr($helper_path, $sIdx + 1);
+        } else {
+            $path   = NULL;
+            $helper = $helper_path;
+        }
+        
+        //Look for the class in the SYSPATH/lib folder if $_inc is TRUE
+        //Or check the APPPATH/lib 
+        $_dir  = (($_inc) ? BASEPATH . '/helper/' : SR_HELPERPATH);
+        $_dir .= $helper_path;
+        
+        foreach( array($_dir . '.helper.php', $_dir . '.php') as $_file ) {
+            if ( file_exists($_file) ) {
+                if ( ! isset($_loadedHelper[$helper_path]) ) {
+                    require $_file;
+                }
+
+                $class = "{$helper}Helper";
+                $obj   = new $class($conf);
+                $_loadedHelper[$helper_path] = $obj;
+                unset($path, $helper, $_dir, $class);
+                return $obj;
+            }
+        }
+        
+        throw new Exception("helper#Unable to load helper with path {$helper_path}");
+    }
+}
+
 ?>
