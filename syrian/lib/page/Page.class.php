b823d42759c3aed07531f14a203f99f5c3eb9d97
Koma <komazhang@foxmail.com>
2016-08-14 14:03:17 +0800

    optimize Page add bootstrap ui

--- syrian/lib/page/Page.class.php
+++ syrian/lib/page/Page.class.php
@@ -17,6 +17,7 @@ defined('UI_NEXT')  or  define('UI_NEXT',           1 << 6);
 defined('UI_LAST')  or  define('UI_LAST',           1 << 7);
 defined('UI_INPUT') or  define('UI_INPUT',          1 << 8);
 defined('UI_INPUT_PAGES')   or  define('UI_INPUT_PAGES',    1 << 9);
+defined('UI_USE_BOOTSTRAP') or  define('UI_USE_BOOTSTRAP',  1 << 10);
 defined('UI_SHOP_STYLE')    or  define('UI_SHOP_STYLE', UI_PREV | UI_LEFT | UI_QUICK | UI_NEXT | UI_INPUT | UI_INPUT_PAGES);
 defined('UI_DEFAULT_STYLE') or  define('UI_DEFAULT_STYLE', UI_TOTAL | UI_PAGES | UI_FIRST | UI_PREV | UI_QUICK | UI_NEXT | UI_LAST);
 
@@ -126,6 +127,17 @@ class Page
         
         //---------------------------------------------------------------
 
+        if ( ($_style & UI_USE_BOOTSTRAP) != 0 ) {
+            $_html = $this->bootstrapUI($_link, $_name, $_style, $_left, $_offset, $_forms);
+        } else {
+            $_html = $this->defaultUI($_style, $_name, $_left, $_offset, $_link, $_forms);
+        }
+        
+        return $_html;
+    }
+
+    private function defaultUI($_link, $_name, $_style, $_left, $_offset, $_forms)
+    {
         $_html = '<div class="ui-page-box">';
         if ( ($_style & UI_TOTAL) != 0 )
             $_html .= '<a class="ui-page-total">'.$this->_lang['total'] . $this->_total.'</a>';
@@ -207,6 +219,101 @@ class Page
             $_html .= '</form>';
         }
         $_html .= '</div>';
+
+        return $_html;
+    }
+
+    //@NOTE this is need admin/common.css
+    private function bootstrapUI($_link, $_name, $_style, $_left, $_offset, $_forms)
+    {
+        $_html = '<nav><ul class="pagination">';
+        if ( ($_style & UI_TOTAL) != 0 )
+            $_html .= '<li><a>'.$this->_lang['total'].$this->_total.'</a></li>';
+        if ( ($_style & UI_PAGES) )
+            $_html .= '<li><a>'.$this->_pageno.'/'.$this->_pages.'</a></li>';
+            
+        if ( ( $_style & UI_FIRST ) != 0 ) {
+            if ( $this->_pageno > 1 )
+                $_html .= '<li><a href="'.$_link.'1">'.$this->_lang['first'].'</a></li>';
+            else
+                $_html .= '<li class="disabled"><a>'.$this->_lang['first'].'</a></li>';
+        }
+        if ( ($_style & UI_PREV) != 0 ) {
+            if ( $this->_pageno > 1 )
+                $_html .= '<li><a href="'.$_link.($this->_pageno-1).'">'.$this->_lang['prev'].'</a></li>';
+            else
+                $_html .= '<li class="disabled"><a>'.$this->_lang['prev'].'</a></li>';
+        }
+        
+        //start quick two handle
+        $_start = $this->_pageno - $_offset;
+        if ( $_start <= 0 ) $_start = 1;
+        
+        if ( ( $_style & UI_LEFT ) != 0 )
+        {
+            if ( $_start > 1 )
+            {
+                $_limit = ($_start == $_left ) ? 1 : $_left;
+                for ( $i = 1; $i <= $_limit; $i++ ) 
+                    $_html .= '<li><a href="'.$_link.$i.'">'.$i.'</a></li>';
+                if ( $_limit != 1 ) $_html .= '<li><a>...</a></li>';
+            }
+        }
+        
+        if ( ( $_style & UI_QUICK ) != 0 )
+        {
+            //prev pages
+            for ( $i = $_start; $i < $this->_pageno; $i++  ) 
+                $_html .= '<li><a href="'.$_link.$i.'">'.$i.'</a></li>';
+            
+            $_html .= '<li class="active"><a href="#">'.$this->_pageno.'</a></li>';
+            
+            $_end = $this->_pageno + $_offset;
+            if ( $_end > $this->_pages ) $_end = $this->_pages;
+            for ( $i = $this->_pageno + 1; $i <= $_end; $i++  ) 
+                $_html .= '<li><a href="'.$_link.$i.'">'.$i.'</a></li>';
+        }
+        
+        if ( ($_style & UI_NEXT) != 0 )
+        {
+            if ( $this->_pageno < $this->_pages ) 
+                $_html .= '<li><a href="'.$_link.($this->_pageno+1).'">'.$this->_lang['next'].'</a></li>';
+            else
+                $_html .= '<li class="disabled"><a>'.$this->_lang['next'].'</a></li>';
+        }
+        
+        if ( ($_style & UI_LAST) != 0 )
+        {
+            if ( $this->_pageno < $this->_pages ) 
+                $_html .= '<li><a href="'.$_link.$this->_pages.'">'.$this->_lang['last'].'</a></li>';
+            else 
+                $_html .= '<li class="disabled"><a>'.$this->_lang['last'].'</a></li>';
+        }
+        
+        
+        if ( ($_style & UI_INPUT) != 0 )
+        {
+            if ( ($_style & UI_INPUT_PAGES) != 0 )
+            $_html .= '<li><a>'.str_replace('{pages}', $this->_pages, $this->_lang['pages']).'</a></li>';
+            $_html .= '<li><a style="width: 180px; padding: 0; height: 31px;">';
+            $_html .= '<form name="ui_page_form" action="'.($_SERVER['REQUEST_URI']).'" method="get" id="ui-page-form">';
+            $_html .= '<div class="input-group input-group-sm syrian-page-form">';
+            $_html .= '<span class="input-group-addon page-msg-start">到第</span>';
+            $_html .= '<input type="text" class="form-control page-msg-input" name="'.$_name.'" value="'.$this->_pageno.'">';
+            $_html .= '<span class="input-group-addon page-msg-end">页</span>';
+            $_html .= '<span class="input-group-btn">';
+            $_html .= '<button class="btn btn-default page-submit-btn" type="button" onclick="if(document.ui_page_form.elements[0].value.match(/([0-9]{1,})/)!=null) document.ui_page_form.submit();">'.$this->_lang['submit'].'</button>';
+            $_html .= '</span>';
+            $_html .= '</div>';
+            if ( $_forms != NULL )
+            {
+                foreach ( $_forms as $_key => $_val )
+                    $_html .= '<input type="hidden" name="'.$_key.'" value="'.$_val.'" />';
+            }
+            $_html .= '</form>';
+            $_html .= '</a></li>';
+        }
+        $_html .= '</ul></nav>';
         
         return $_html;
     }
