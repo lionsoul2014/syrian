8804d73453620bfce6666fc346977a115f1e9649
lionsoul <chenxin619315@gmail.com>
2017-09-24 21:08:11 +0800

    Utf8String class forward

--- syrian/lib/util/Utf8String.class.php
+++ syrian/lib/util/Utf8String.class.php
@@ -12,12 +12,13 @@ class Utf8String
     /**
      * get the bytes of the speicifield UTF-8 character
      *
-     * @param   $char
+     * @param   $str
+     * @param   $i
      * @return  int the bytes that this char takes
     */
-    public static function charBytes($char)
+    public static function charBytes($str, $i)
     {
-        $char = ord($char);
+        $char = ord($str[$i]);
         $byts = 0;
 
         if  ( ($char & 0x80) == 0 ) return 1;
@@ -49,7 +50,7 @@ class Utf8String
         
         //do the string filter
         for ( $i = 0; $i < $len; ) {
-            $bytes = self::getCharBytes($string[$i]);
+            $bytes = self::charBytes($string, $i);
             if ( $bytes <= $mbytes ) {
                 $ret .= substr($string, $i, $bytes);
             }
@@ -66,34 +67,34 @@ class Utf8String
      * @param  $char
      * @return int
     */
-    public static function unicode($char) 
+    public static function unicode($str, $i, &$bytes=-1) 
     {
-        $code  = 0;
-        $bytes = self::charBytes($char[0]);
+        if ( $bytes == -1 ) {
+            $bytes = self::charBytes($str, $i);
+        }
 
         switch ( $bytes ) {
         case 1:
-            $code = ord($char);
-            break;
+            return ord($str[$i]);
         case 2:
-            $b1   = ord($char[0]);
-            $b2   = ord($char[1]);
+            $b1   = ord($str[$i]);
+            $b2   = ord($str[$i+1]);
 
-            $by_0 = ( (($b1 << 6) + ($b2 & 0x3F)) & 0x7F );
-            $by_1 = ( (($b1 >> 2) & 0x07) & 0x7F );
+            $by_0 = ( (($b1 << 6) + ($b2 & 0x3F)) & 0xFF );
+            $by_1 = ( (($b1 >> 2) & 0x07) & 0xFF );
             return (($by_1 << 8) | $by_0);
         case 3:
-            $b1   = ord($char[0]);
-            $b2   = ord($char[1]);
-            $b3   = ord($char[2]);
+            $b1   = ord($str[$i]);
+            $b2   = ord($str[$i+1]);
+            $b3   = ord($str[$i+2]);
 
-            $by_0 = ( (($b2 << 6) + ($b3 & 0x3F)) & 0x7F);
-            $by_1 = ( (($b1 << 4) + (($b2 >> 2) & 0x0F)) & 0x7F);
+            $by_0 = ( (($b2 << 6) + ($b3 & 0x3F)) & 0xFF);
+            $by_1 = ( (($b1 << 4) + (($b2 >> 2) & 0x0F)) & 0xFF);
             return (($by_1 << 8) | $by_0);
             //ignore the ones that are larger than 3 bytes;
         }
 
-        return $code;
+        return 3;
     }
 
     //turn the unicode serial to a utf-8 string.
@@ -154,5 +155,133 @@ class Utf8String
 
         return $str;
     }
-    
+
+    /**
+     * check the specifield char is punctuation or not
+     * both english and chinese punctuation will be ok
+     *
+     * @param   $u
+     * @return  boolean
+    */
+    public static function isPunctuation($u)
+    {
+        if ( $u > 65280 ) {
+            $u -= 65248;
+        }
+
+        return ( 
+            ($u > 32 && $u  < 48 ) || 
+            ($u > 57 && $u  < 65 ) || 
+            ($u > 90 && $u  < 97 ) || 
+            ($u > 122 && $u < 127)
+        );
+    }
+
+    /**
+     * check if the specifield is a whitespace
+
+     * @param   $u
+     * @return  boolean
+    */
+    public static function isWhitespace($u)
+    {
+        if ( $u > 65280 ) {
+            $u -= 65248;
+        }
+
+        return ($u == 32);
+    }
+
+    /*
+     * check the given char is a english punctuation.
+     * 
+     * @param   $u
+     * @return  boolean
+    */
+    public static function isENPunctuation($u) 
+    {
+        return ( 
+            ($u > 32 && $u  < 48 ) || 
+            ($u > 57 && $u  < 65 ) || 
+            ($u > 90 && $u  < 97 ) || 
+            ($u > 122 && $u < 127)
+        );
+    }
+
+    /*
+     * check the given char is a chinese punctuation.
+     * @date    2013-08-31 added.
+     *
+     * @param   $u
+     * @return  boolean
+    */
+    public static function isCNPunctuation($u) 
+    {
+        return (
+            ($u > 65280 && $u < 65296 ) || 
+            ($u > 65305 && $u < 65312 ) || 
+            ($u > 65338 && $u < 65345 ) || 
+            ($u > 65370 && $u < 65382 ) ||
+                //cjk symbol and p$unct$uation.(added 2013-09-06)
+                //from http://www.$unicode.org/charts/PDF/U3000.pdf
+            ($u >= 12289 && $u <= 12319)
+        );
+    }
+
+    /**
+     * check if the current char is chinese letter or not
+     * 
+     * @param   $u
+     * @param   $numeric
+     * @return  boolean
+    */
+    public static function isEnglishLetter($u)
+    {
+        if ( $u > 65280 ) {
+            $u -= 65248;
+        }
+
+        return (
+            ($u >= 48 && $u <= 57 ) ||
+            ($u >= 65 && $u <= 90 ) ||
+            ($u >= 97 && $u <= 122)
+        );
+    }
+
+    /**
+     * check if the current char is chinese letter or not
+     * 
+     * @param   $u
+     * @return  boolean
+    */
+    public static function isChineseLetter($u)
+    {
+        $c = ( 
+            ($u >= 0x4E00 && $u <= 0x9FBF) 
+            || ($u >= 0x2E80 && $u <= 0x2EFF) 
+            || ($u >= 0x2F00 && $u <= 0x2FDF) 
+            || ($u >= 0x31C0 && $u <= 0x31EF) 
+            //|| ( $u >= 0x3200 && $u <= 0x32FF )
+            || ($u >= 0x3300 && $u <= 0x33FF) 
+            //|| ( $u >= 0x3400 && $u <= 0x4DBF )
+            || ($u >= 0x4DC0 && $u <= 0x4DFF) 
+            || ($u >= 0xF900 && $u <= 0xFAFF)
+            || ($u >= 0xFE30 && $u <= 0xFE4F)
+        ); 
+
+        $j = (
+            ($u >= 0x3040 && $u <= 0x309F)
+            || ($u >= 0x30A0 && $u <= 0x30FF) 
+            || ($u >= 0x31F0 && $u <= 0x31FF)
+        );
+
+        $k = (
+            ($u >= 0xAC00 && $u <= 0xD7AF)
+            || ($u >= 0x1100 && $u <= 0x11FF) 
+            || ($u >= 0x3130 && $u <= 0x318F)
+        );
+
+        return ($c || $j || $c);
+    }
+
 }
